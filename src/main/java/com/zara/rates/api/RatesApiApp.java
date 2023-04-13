2e45e8723245b6c4919ee93732807773bb336c8c
guidogranobles <guido-granobles@hotmail.com>
2023-04-13 17:01:02 +0200

    Project initialized, added dependencies and structure

---
+++ src/main/java/com/zara/rates/api/RatesApiApp.java
@@ -0,0 +1,13 @@
+package com.zara.rates.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatesApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatesApiApp.class, args);
+    }
+}
+
