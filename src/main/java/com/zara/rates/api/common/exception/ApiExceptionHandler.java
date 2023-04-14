c8262381d94ac087da5eee51053b68d09be541c6
guidogranobles <guido-granobles@hotmail.com>
2023-04-14 21:38:11 +0200

    Added database config, flayway config, exception handler, openApi endpoint def.

---
+++ src/main/java/com/zara/rates/api/common/exception/ApiExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.zara.rates.api.common.exception;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+    private static final Logger loggerException = LogManager.getLogger(ApiExceptionHandler.class);
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        loggerException.error(ex);
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({GeneralException.class})
+    public ResponseEntity generalExceptionHandler(GeneralException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        loggerException.error(ex);
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/main/java/com/zara/rates/api/common/exception/GeneralException.java
@@ -0,0 +1,16 @@
+package com.zara.rates.api.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+public class GeneralException extends Exception {
+
+    public GeneralException(String message) {
+        super(message);
+    }
+
+    public GeneralException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
